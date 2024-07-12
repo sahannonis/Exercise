f55a295d08a6c271fe01128cb7e2982e3aed6f1e
Pattinikuttige Nonis <pattinikuttiges@student.douglascollege.ca>
2024-07-12 12:17:09 -0700

    last correction

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ let num2 = prompt("Enter the second Number: ");
 if(!isNaN(num1) || !isNaN(num2) && num2 != 0){
 let sum = parseInt(num1) + parseInt(num2);
 document.write(`${num1} + ${num2} = `, sum);
-document.write(`<br>${num1} - ${num2} = `, num1 - num2);
-document.write(`<br>${num1} * ${num2} = `, num1 * num2);
-document.write(`<br>${num1} / ${num2} = `, num1 / num2);
+document.write(`<br>${num1}-${num2} = `, num1 - num2);
+document.write(`<br>${num1}*${num2} = `, num1 * num2);
+document.write(`<br>${num1}/${num2} = `, num1 / num2);
 
 isValid = true;
 }
